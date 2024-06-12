13dd9dca83a30685f0b6801b8d1115a50303d2f4
Your Name <you@example.com>
2024-06-12 18:57:44 -0400

    comando

--- build_mlx.sh
+++ build_mlx.sh
@@ -2,3 +2,6 @@ git clone https://github.com/codam-coding-college/MLX42.git
 cd MLX42
 cmake -B build # build here refers to the outputfolder.
 cmake --build build -j4 # or do make -C build -j4
+
+sudo apt update
+sudo apt install build-essential libx11-dev libglfw3-dev libglfw3 xorg-dev
