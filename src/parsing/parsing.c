e0409be8b5a8a15657be201ac9dd25cf6637cbaf
Your Name <you@example.com>
2024-07-30 20:50:04 -0400

    circulo'

--- src/parsing/parsing.c
+++ src/parsing/parsing.c
@@ -6,6 +6,30 @@
 
 #include "../../inc/fractol.h"
 
+int32_t ft_pixel(int32_t r, int32_t g, int32_t b, int32_t a)
+{
+    return (r << 24 | g << 16 | b << 8 | a);
+}
+void drawCircle(t_data *db, int centerX, int centerY, int radius) 
+{
+	int y = -radius;
+
+	while (y <= radius) 
+	{
+		int x = -radius;
+		while (x <= radius)
+		{
+			if (x*x + y*y <= radius*radius)
+			{
+				mlx_put_pixel(db->img, centerX + x, centerY + y, ft_pixel(255, 90, 71, 255));
+			}
+			x++;
+		}
+		y++;
+	}
+}
+
+
 char *ft_strtolowel(char *str)
 {
 	int i= 0;
@@ -25,11 +49,13 @@ int ft_parsing(int ac, char **av, t_data *db)
 		if ((ft_strncmp(av[1], "julia", 5) == 0) && (ft_strlen(av[1]) == 5))
 		{
 			ft_putstr_fd("LOOK JULIA", 1);
-			return(0);
+			db->algo_type = 1;
+			return(0); 
 		}
 		else if((ft_strncmp(av[1], "mandel", 6) == 0) && (ft_strlen(av[1]) == 6))
 		{
 			ft_putstr_fd("LOOK MANDEL", 1);
+			db->algo_type = 2;
 			return(0);
 		}
 	}
@@ -47,7 +73,20 @@ int main(int ac, char **av)
 	t_data *db;
 
 	db = ft_calloc(sizeof(t_data), 1);
-	ft_parsing(ac, av, db);
+	db->algo_type = 0;
+	if(ft_parsing(ac, av, db) == 0)
+	{
+		printf("\n DESPUES %d\n", db->algo_type);
+		db->mlx = mlx_init(WIDTH, HEIGHT, "MLX42",false);
+		db->img = mlx_new_image(db->mlx, WIDTH, HEIGHT);
+		mlx_image_to_window(db->mlx, db->img, 0, 0);
+		mlx_put_pixel(db->img, 1, 1, -1);
+		drawCircle(db, 206, 206, 20);
+		mlx_loop(db->mlx);
+		mlx_terminate(db->mlx);
+
+
+	}  
 	return (0);
 }
 
